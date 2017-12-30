fa60931b9fc6e1161c5259bf9832d95c08b81740
Kajornjit Songsaen <kajornjit@gmail.com>
2017-12-30 20:03:45 +0700

    first commit

---
+++ basic.rb
@@ -0,0 +1,18 @@
+def greeting
+    out = "Hello World"
+    puts out
+end
+
+class Customer
+    @@no_of_customers = 0
+    def initialize(id, name, addr)
+       @cust_id = id
+       @cust_name = name
+       @cust_addr = addr
+       puts @cust_id + ', ' + @cust_name + ', ' + @cust_addr
+    end
+ end
+
+ greeting
+ cust1 = Customer.new("1", "John", "Wisdom Apartments, Ludhiya")
+ cust2 = Customer.new("2", "Poul", "New Empire road, Khandala")
